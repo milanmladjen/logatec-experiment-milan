c014fbf82cfba26996d700116ab2f84d8f37d9b6
CiucIJS <gregaa.morano@gmail.com>
2020-09-18 10:06:30 +0200

    Removed TEST enviromental variable

--- deploy/docker/start.sh
+++ deploy/docker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set test
-if [ "$TEST" = "" ] || [ "$TARGET" = "" ]; then
-  echo "Test missing!"
+if [ "$TARGET" = "" ]; then
+  echo "Target missing!"
 else
   if [[ "$TARGET": = *"agent"* ]]; then
     # Set serial device
@@ -10,7 +10,7 @@ else
       echo "Serial device missing!"
     fi
   fi
-  cd "$TEST"
+  cd deploy/tasks/
   pwd 
   make "$TARGET"
 fi
