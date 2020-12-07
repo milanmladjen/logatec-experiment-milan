182172417ab295c76cbfdf5e6a55ac6549099165
CiucIJS <gregaa.morano@gmail.com>
2020-12-07 11:24:40 +0100

    Rearrangement

--- monitor/client.py
+++ monitor/client.py
@@ -36,150 +36,6 @@ def obtain_info(cmd):
 
     return data
 
-def main():
-
-    # First sync with the server, so it knows we are online
-    logging.debug("Send a synchronization request")
-
-    packet_type = b"SYNC"
-    client.send_multipart([packet_type, b"Hi"])
-
-    packet_type, msg = client.recv_multipart()
-    if packet_type == "SYNC":
-        logging.info("Synced with server (%s)" % msg)
-    else:
-        logging.error("Could not sync with server!")
-
-
-# ------------------------------------------------------------------------------- #
-# Devices are synchronized - start the app
-# ------------------------------------------------------------------------------- #
-
-    waiting_for_ack = False
-    last_sent_info = b""
-    last_sent_time = 0
-    
-    rx_msg_nbr = 0
-    rx_pub_nbr = 0
-    tx_msg_nbr = 0
-    retires_nbr = 0
-
-    while True:
-        # Check for any incoming messages
-        socks = dict(poller.poll(0))
-        
-        # If there are any command_messages from the publish server
-        if socks.get(subscriber) == zmq.POLLIN:
-
-            # Read them
-            msg = subscriber.recv()
-
-            msg_s = msg.split()
-            rx_pub_nbr = int(msg_s[0])
-            msg = msg_s[1]
-
-            logging.info("Received PUB_CMD [%i]: %s" % (rx_pub_nbr, msg))
-            
-            # Obtain the info upon received command 
-            info = obtain_info(msg)
-
-            # Form reply
-            reply = ["PUB_DAT", str(rx_pub_nbr), info] 
-
-            # Respond to the server
-            logging.debug("Sending data...")
-            client.send_multipart(reply)
-
-            # If waiting is still true, that means that server did not hear us until now
-            if waiting_for_ack:
-                logging.warning("Got new command but server didn't receive our previous message!")
-                # logging.warning("Old message will be discarted.. :/")
-                # If user needs that message again, he will request for it.. 
-            
-            waiting_for_ack = rx_pub_nbr
-            last_sent_info = reply
-            last_sent_time = timer.now()
-
-            # Maybe add a just a bit of delay here. Because without it, client won't receive the ACK right
-            # away and will go work some other stuff. Which is ok, but it must come here fast enough (rest 
-            # of the code shouldn't delay for too long)
-
-            # Or maybe use "continue" to return to poller check on the beginning
-            # Beware that then you can stuck here if you got many messages in queue
-            continue
-
-        
-        # If we received any direct messages from router server
-        if socks.get(client) == zmq.POLLIN:
-            # Read them
-            msg_type, rx_msg_nbr, msg = client.recv_multipart()
-
-            # If we got acknowledge on transmitted data
-            if msg_type == "DATA_ACK":
-                if int(rx_msg_nbr) == waiting_for_ack:
-                    logging.info("Server acknowledged our data " + rx_msg_nbr)
-                    waiting_for_ack = None
-                    retires_nbr = 0
-                else:
-                    # This shouldn't occur to many times - here for possible upgrade
-                    # It is rare because if we send new data before we get prev ack, int waiting_for_ack will 
-                    # be overwritten...but it still can happen if we overwrite message and receive prev ack right after it
-                    logging.warning("Got ACK for msg %s instead of %s.." % (rx_msg_nbr, waiting_for_ack))
-                    waiting_for_ack = None
-                    retires_nbr = 0
-
-            elif msg_type == "UNI_CMD":
-                logging.info("Received UNI_CMD [%s]: %s" % (rx_msg_nbr,msg))
-
-                # Obtain the info upon received command 
-                info = obtain_info(msg)
-
-                # Form reply
-                reply = ["UNI_DAT", rx_msg_nbr, info]
-
-                # Respond to the server
-                logging.debug("Sending data...")
-                client.send_multipart(reply)
-
-                # If waiting is still true, that means that server did not hear us until now
-                if waiting_for_ack:
-                    logging.warning("Got new command but server didn't ack our previous message!")
-                    # logging.warning("Old message will be discarted.. :/")
-                    # If user needs that message again, he will request for it.. 
-                
-                waiting_for_ack = rx_pub_nbr
-                last_sent_info = reply
-                last_sent_time = timer.now()
-
-            else:
-                loging.warning("Received unknown type of message...discarting.")
-
-
-        # If we sent one message and there was no response for more than a second, resend it
-        if waiting_for_ack > 0:
-            # Check how long we waited for ACK - if second passed
-            time_now = timer.now()
-            if ((time_now - last_sent_time).total_seconds() > 1):
-                logging.warning("Second has passed and no response from server.. Resending data!")
-                # Resend info
-                client.send_multipart(last_sent_info)
-                waiting_for_ack = last_sent_info[1] # tx_msg_nbr
-                last_sent_time = timer.now()
-
-                retires_nbr += 1
-                if retires_nbr > 2:
-                    # Server has died ?
-                    waiting_for_ack = None
-                    retires_nbr = 0
-                    logging.warning("Server has died :(")
-
-        
-        # Do some other stuff
-        print(".")
-        #logging.debug(".")
-        time.sleep(0.1)
-
-
 
 
 # ===================================================================================== #
@@ -213,4 +69,149 @@ poller = zmq.Poller()
 poller.register(subscriber, zmq.POLLIN)
 poller.register(client, zmq.POLLIN)
 
-main()
+# ------------------------------------------------------------------------------- #
+# Synchronize with the server
+# ------------------------------------------------------------------------------- #
+logging.debug("Send a synchronization request")
+
+packet_type = b"SYNC"
+client.send_multipart([packet_type, b"Hi"])
+
+packet_type, msg = client.recv_multipart()
+if packet_type == "SYNC":
+    logging.info("Synced with server (%s)" % msg)
+else:
+    logging.error("Could not sync with server!")
+
+
+# ------------------------------------------------------------------------------- #
+# Devices are synchronized - start the app
+# ------------------------------------------------------------------------------- #
+
+waiting_for_ack = False
+last_sent_info = b""
+last_sent_time = 0
+
+rx_msg_nbr = 0
+rx_pub_nbr = 0
+tx_msg_nbr = 0
+retires_nbr = 0
+
+while True:
+    # Check for any incoming messages
+    socks = dict(poller.poll(0))
+    
+    # If there are any command_messages from the publish server
+    if socks.get(subscriber) == zmq.POLLIN:
+
+        # Read them
+        msg = subscriber.recv()
+
+        msg_s = msg.split()
+        rx_pub_nbr = int(msg_s[0])
+        msg = msg_s[1]
+
+        logging.info("Received PUB_CMD [%i]: %s" % (rx_pub_nbr, msg))
+        
+        # Obtain the info upon received command 
+        info = obtain_info(msg)
+
+        # Form reply
+        reply = ["PUB_DAT", str(rx_pub_nbr), info] 
+
+        # Respond to the server
+        logging.debug("Sending data...")
+        client.send_multipart(reply)
+
+        # If waiting is still true, that means that server did not hear us until now
+        if waiting_for_ack:
+            logging.warning("Got new command but server didn't receive our previous message!")
+            # logging.warning("Old message will be discarted.. :/")
+            # If user needs that message again, he will request for it.. 
+        
+        waiting_for_ack = rx_pub_nbr
+        last_sent_info = reply
+        last_sent_time = timer.now()
+
+        # Maybe add a just a bit of delay here. Because without it, client won't receive the ACK right
+        # away and will go work some other stuff. Which is ok, but it must come here fast enough (rest 
+        # of the code shouldn't delay for too long)
+
+        # Or maybe use "continue" to return to poller check on the beginning
+        # Beware that then you can stuck here if you got many messages in queue
+        continue
+
+    
+    # If we received any direct messages from router server
+    if socks.get(client) == zmq.POLLIN:
+        # Read them
+        msg_type, rx_msg_nbr, msg = client.recv_multipart()
+
+        # If we got acknowledge on transmitted data
+        if msg_type == "DATA_ACK":
+            if int(rx_msg_nbr) == waiting_for_ack:
+                logging.info("Server acknowledged our data " + rx_msg_nbr)
+                waiting_for_ack = None
+                retires_nbr = 0
+            else:
+                # This shouldn't occur to many times - here for possible upgrade
+                # It is rare because if we send new data before we get prev ack, int waiting_for_ack will 
+                # be overwritten...but it still can happen if we overwrite message and receive prev ack right after it
+                logging.warning("Got ACK for msg %s instead of %s.." % (rx_msg_nbr, waiting_for_ack))
+                waiting_for_ack = None
+                retires_nbr = 0
+
+        elif msg_type == "UNI_CMD":
+            logging.info("Received UNI_CMD [%s]: %s" % (rx_msg_nbr,msg))
+
+            # Obtain the info upon received command 
+            info = obtain_info(msg)
+
+            # Form reply
+            reply = ["UNI_DAT", rx_msg_nbr, info]
+
+            # Respond to the server
+            logging.debug("Sending data...")
+            client.send_multipart(reply)
+
+            # If waiting is still true, that means that server did not hear us until now
+            if waiting_for_ack:
+                logging.warning("Got new command but server didn't ack our previous message!")
+                # logging.warning("Old message will be discarted.. :/")
+                # If user needs that message again, he will request for it.. 
+            
+            waiting_for_ack = rx_pub_nbr
+            last_sent_info = reply
+            last_sent_time = timer.now()
+
+        else:
+            loging.warning("Received unknown type of message...discarting.")
+
+
+    # If we sent one message and there was no response for more than a second, resend it
+    if waiting_for_ack > 0:
+        # Check how long we waited for ACK - if second passed
+        time_now = timer.now()
+        if ((time_now - last_sent_time).total_seconds() > 1):
+            logging.warning("Second has passed and no response from server.. Resending data!")
+            # Resend info
+            client.send_multipart(last_sent_info)
+            waiting_for_ack = last_sent_info[1] # tx_msg_nbr
+            last_sent_time = timer.now()
+
+            retires_nbr += 1
+            if retires_nbr > 2:
+                # Server has died ?
+                waiting_for_ack = None
+                retires_nbr = 0
+                logging.warning("Server has died :(")
+
+    
+    # Do some other stuff
+    print(".")
+    #logging.debug(".")
+    time.sleep(0.1)
+
+
+
+
--- monitor/server.py
+++ monitor/server.py
@@ -7,85 +7,6 @@ NUMBER_OF_DEVICES = 3
 
 lgtc_addr = []
 
-def main():
-    
-    # First wait for synchronization from all subscribers
-    subscribers = 0
-    while subscribers < NUMBER_OF_DEVICES:
-        # Wait for synchronization request (msg is "Hi")
-        #address, msg = router.recv_multipart()
-        address, packet_type, msg = router.recv_multipart()
-
-        # Add address to the list of LGTC addr
-        if address not in lgtc_addr:
-            lgtc_addr.append(address)
-
-        # Send synchronization reply
-        resp = "Hello there!"
-        router.send_multipart([address, b"SYNC", resp])
-
-        # Inform user about new device
-        subscribers += 1
-        logging.info("Device %s online (%i/%i)" % (address, subscribers, NUMBER_OF_DEVICES))
-
-    
-# ------------------------------------------------------------------------------- #
-# Devices are synchronized - start the app
-# ------------------------------------------------------------------------------- #
-
-    # Start pushing commands
-    tx_msg_nbr = 0
-    for i in range(60):
-        
-
-        # Push command
-        if(i%6 == 0):
-            tx_msg_nbr += 1
-
-            cmd = "Do-smth"
-
-            msg =b"%i %s" % (tx_msg_nbr, cmd)
-            publisher.send(msg)
-            logging.debug("Sent PUB_CMD [%i]: %s" % (tx_msg_nbr, cmd))
-
-        if(i%8 == 0):
-            adr = lgtc_addr[0]
-            router.send_multipart([adr, b"UNI_CMD", b"1", b"STATE"])
-
-        # Check if we got any response from devices
-        while True:
-            
-            # Wait just a bit (10ms) to check if there is more incomeing messages
-            # If we go ahead too fast, we will miss them and client has to wait for us to come here again
-            # LGTC needs aprox 30ms if not even more
-            # But if we will go through the rest of the code fast, we don't need this timeout
-            # Problem is that now we wait here for 30ms or even more each time we get here..not good
-
-            socks = dict(poller.poll(100))
-
-            # If there is any message in pollin queue
-            if socks.get(router) == zmq.POLLIN:
-                address, msg_type, rx_msg_nbr, msg = router.recv_multipart()
-                router.send_multipart([address, b"DATA_ACK", rx_msg_nbr, b""])
-
-                logging.debug("%s sent: %s" % (address, msg))
-
-                #tx_msg_nbr += 1
-                #router.send_multipart([address, b"UNI_CMD", tx_msg_nbr, b""])
-
-            # No response from devices
-            else:
-                break
-
-        print(".")
-        time.sleep(0.5)
-    
-    tx_msg_nbr += 1
-    cmd = "END"
-    msg =b"%i %s" % (tx_msg_nbr, cmd)
-    # Send stop command
-    publisher.send(msg)
-
 
 # ===================================================================================== #
 # Configure logging module
@@ -109,4 +30,84 @@ router.bind('tcp://*:5562')
 poller = zmq.Poller()
 poller.register(router, zmq.POLLIN)
 
-main()
+
+# ------------------------------------------------------------------------------- #
+# First wait for synchronization from all subscribers
+# ------------------------------------------------------------------------------- #
+subscribers = 0
+while subscribers < NUMBER_OF_DEVICES:
+    # Wait for synchronization request (msg is "Hi")
+    #address, msg = router.recv_multipart()
+    address, packet_type, msg = router.recv_multipart()
+
+    # Add address to the list of LGTC addr
+    if address not in lgtc_addr:
+        lgtc_addr.append(address)
+
+    # Send synchronization reply
+    resp = "Hello there!"
+    router.send_multipart([address, b"SYNC", resp])
+
+    # Inform user about new device
+    subscribers += 1
+    logging.info("Device %s online (%i/%i)" % (address, subscribers, NUMBER_OF_DEVICES))
+
+
+# ------------------------------------------------------------------------------- #
+# Devices are synchronized - start the app
+# ------------------------------------------------------------------------------- #
+
+# Start pushing commands
+tx_msg_nbr = 0
+for i in range(60):
+    
+
+    # Push command
+    if(i%6 == 0):
+        tx_msg_nbr += 1
+
+        cmd = "Do-smth"
+
+        msg =b"%i %s" % (tx_msg_nbr, cmd)
+        publisher.send(msg)
+        logging.debug("Sent PUB_CMD [%i]: %s" % (tx_msg_nbr, cmd))
+
+    if(i%8 == 0):
+        adr = lgtc_addr[0]
+        router.send_multipart([adr, b"UNI_CMD", b"1", b"STATE"])
+
+    # Check if we got any response from devices
+    while True:
+        
+        # Wait just a bit (10ms) to check if there is more incomeing messages
+        # If we go ahead too fast, we will miss them and client has to wait for us to come here again
+        # LGTC needs aprox 30ms if not even more
+        # But if we will go through the rest of the code fast, we don't need this timeout
+        # Problem is that now we wait here for 30ms or even more each time we get here..not good
+
+        socks = dict(poller.poll(100))
+
+        # If there is any message in pollin queue
+        if socks.get(router) == zmq.POLLIN:
+            address, msg_type, rx_msg_nbr, msg = router.recv_multipart()
+            router.send_multipart([address, b"DATA_ACK", rx_msg_nbr, b""])
+
+            logging.debug("%s sent: %s" % (address, msg))
+
+            #tx_msg_nbr += 1
+            #router.send_multipart([address, b"UNI_CMD", tx_msg_nbr, b""])
+
+        # No response from devices
+        else:
+            break
+
+    print(".")
+    time.sleep(0.5)
+
+tx_msg_nbr += 1
+cmd = "END"
+msg =b"%i %s" % (tx_msg_nbr, cmd)
+# Send stop command
+publisher.send(msg)
+
+
