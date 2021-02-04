499f78298440761128db66d96b2500c475dada01
CiucIJS <gregaa.morano@gmail.com>
2021-02-04 14:36:36 +0100

    Test

--- monitoring/flask-server.py
+++ monitoring/flask-server.py
@@ -19,7 +19,7 @@ experiment_started = False
 lock = Lock()
 
 # Flask and SocketIO config
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="monitoring/static", template_folder="monitoring/templates")
 socketio = SocketIO(app, async_mode=None)
 
 thread = Thread()
