1e852c0f51f419668eb374a2cb6e1e4b1bc64e03
9morano <9morano@IJS-TP>
2021-02-22 15:23:59 +0100

    Test

--- monitoring/fserver.py
+++ monitoring/fserver.py
@@ -102,7 +102,9 @@ def zmqThread():
     context = zmq.Context()
     zmq_soc = context.socket(zmq.DEALER)
     zmq_soc.setsockopt(zmq.IDENTITY, b"flask_process")
-    zmq_soc.connect("ipc:///tmp/zmq_ipc")
+    #zmq_soc.connect("ipc:///tmp/zmq_ipc")      # local test (on PC)
+    zmq_soc.connect("tcp://192.168.88.239:5563")# local test (Docker)
+    #zmq_soc.connect("tcp://193.2.205.19:5563") # testbed 
 
     poller = zmq.Poller()
     poller.register(zmq_soc, zmq.POLLIN)
