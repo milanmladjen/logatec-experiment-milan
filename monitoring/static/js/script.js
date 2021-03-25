9ea6bf41feeb4363bdea2214481bb121bb9de2fc
9morano <gregaa.morano@gmail.com>
2021-03-25 15:18:26 +0100

    UI added nodes positions and IP address

--- monitoring/static/js/script.js
+++ monitoring/static/js/script.js
@@ -5,6 +5,122 @@ var experiment_started = 0;
 
 var available_devices = [];
 
+
+
+// ------------------------------------------------------------------------------------------------------------
+// Testbed tloris animation
+// ------------------------------------------------------------------------------------------------------------
+
+class Nodes {
+
+    constructor(){
+        this.testbed_devices = [
+            // SRDA
+            {name:"LGTC51", address:"192.168.12.51", location:1, version:"SRDA"},
+            {name:"LGTC52", address:"192.168.12.52", location:2, version:"SRDA"},
+            {name:"LGTC53", address:"192.168.12.53", location:3, version:"SRDA"},
+            {name:"LGTC54", address:"192.168.12.54", location:4, version:"SRDA"},
+            {name:"LGTC55", address:"192.168.12.55", location:5, version:"SRDA"},
+            {name:"LGTC56", address:"192.168.12.56", location:6, version:"SRDA"},
+            {name:"LGTC57", address:"192.168.12.57", location:7, version:"SRDA"},
+            {name:"LGTC58", address:"192.168.12.58", location:9, version:"SRDA"},
+            {name:"LGTC59", address:"192.168.12.59", location:11, version:"SRDA"},
+            {name:"LGTC60", address:"192.168.12.60", location:13, version:"SRDA"},
+            {name:"LGTC61", address:"192.168.12.61", location:15, version:"SRDA"},
+            {name:"LGTC62", address:"192.168.12.62", location:16, version:"SRDA"},
+            {name:"LGTC63", address:"192.168.12.63", location:18, version:"SRDA"},
+            {name:"LGTC64", address:"192.168.12.64", location:19, version:"SRDA"},
+            {name:"LGTC65", address:"192.168.12.65", location:20, version:"SRDA"},
+            {name:"LGTC66", address:"192.168.12.66", location:21, version:"SRDA"},
+            {name:"LGTC67", address:"192.168.12.67", location:22, version:"SRDA"},
+            {name:"LGTC68", address:"192.168.12.68", location:23, version:"SRDA"},
+            {name:"LGTC69", address:"192.168.12.69", location:24, version:"SRDA"},
+            {name:"LGTC70", address:"192.168.12.70", location:25, version:"SRDA"},
+            {name:"LGTC71", address:"192.168.12.71", location:26, version:"SRDA"},
+        
+            // SRDB
+            {name:"LGTC81", address:"192.168.12.81", location:1, version:"SRDB"},
+            {name:"LGTC82", address:"192.168.12.82", location:2, version:"SRDB"},
+            {name:"LGTC83", address:"192.168.12.83", location:3, version:"SRDB"},
+            {name:"LGTC84", address:"192.168.12.84", location:4, version:"SRDB"},
+            {name:"LGTC85", address:"192.168.12.85", location:5, version:"SRDB"},
+            {name:"LGTC86", address:"192.168.12.86", location:6, version:"SRDB"},
+            {name:"LGTC87", address:"192.168.12.87", location:7, version:"SRDB"},
+            {name:"LGTC88", address:"192.168.12.88", location:9, version:"SRDB"},
+            {name:"LGTC89", address:"192.168.12.89", location:11, version:"SRDB"},
+            {name:"LGTC90", address:"192.168.12.90", location:13, version:"SRDB"},
+            {name:"LGTC91", address:"192.168.12.91", location:15, version:"SRDB"},
+            {name:"LGTC92", address:"192.168.12.92", location:16, version:"SRDB"},
+            {name:"LGTC93", address:"192.168.12.93", location:18, version:"SRDB"},
+            {name:"LGTC94", address:"192.168.12.94", location:19, version:"SRDB"},
+            {name:"LGTC95", address:"192.168.12.95", location:20, version:"SRDB"},
+            {name:"LGTC96", address:"192.168.12.96", location:21, version:"SRDB"},
+            {name:"LGTC97", address:"192.168.12.97", location:22, version:"SRDB"},
+            {name:"LGTC98", address:"192.168.12.98", location:23, version:"SRDB"},
+            {name:"LGTC99", address:"192.168.12.99", location:24, version:"SRDB"},
+            {name:"LGTC100", address:"192.168.12.100", location:25, version:"SRDB"},
+            {name:"LGTC101", address:"192.168.12.101", location:26, version:"SRDB"},
+        
+            // LPWA
+            {name:"LGTC111", address:"192.168.12.111", location:1, version:"LPWA"},
+            {name:"LGTC112", address:"192.168.12.112", location:6, version:"LPWA"},
+            {name:"LGTC113", address:"192.168.12.113", location:20, version:"LPWA"},
+            {name:"LGTC114", address:"192.168.12.114", location:27, version:"LPWA"},
+        
+            // UWB
+            {name:"LGTC141", address:"192.168.12.141", location:1, version:"UWB"},
+            {name:"LGTC142", address:"192.168.12.142", location:2, version:"UWB"},
+            {name:"LGTC143", address:"192.168.12.143", location:3, version:"UWB"},
+            {name:"LGTC144", address:"192.168.12.144", location:4, version:"UWB"},
+            {name:"LGTC145", address:"192.168.12.145", location:5, version:"UWB"},
+            {name:"LGTC146", address:"192.168.12.146", location:6, version:"UWB"}
+        ];
+
+        this.SRDA_loc = [1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+        this.SRDB_loc = [1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+        this.UWB_loc  = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 17];
+        this.LPWA_loc = [1, 6, 20, 27];
+    }
+
+
+    _get_dev_loc(n){
+        for (var i=0; i<this.testbed_devices.length; i++){
+            if (this.testbed_devices[i].name == n){
+                return this.testbed_devices[i].location;
+            }
+        }
+    }
+
+    _get_dev_ip(n){
+        for (var i=0; i<this.testbed_devices.length; i++){
+            if (this.testbed_devices[i].name == n){
+                return this.testbed_devices[i].address;
+            }
+        }
+    }
+
+
+
+    show_dev(name){
+        document.getElementById("node_" + this._get_dev_loc(name)).style.display = "block";
+    }
+
+    hide_dev(name){
+        document.getElementById("node_" + this._get_dev_loc(name)).style.display = "none";
+    }
+
+    hide_all(){
+        for(let i=1; i<28; i++){
+            document.getElementById("node_" + i).style.display = "none";
+        }
+    }
+
+    update_dev(name, color){
+        document.getElementById("node_" + this.dev_loc(name)).style.color = color;
+    }
+}
+
+
 function dropdownAddDevice(dev){
     
     // Create new option (value = dd_LGTC66) because id LGTC66 is used elsewhere
@@ -33,7 +149,7 @@ function dropdownDeleteDevice(dev){
 
 // Check if input command is in list of supported commands
 function commandSupported(c){
-    system_commands = [
+    var system_commands = [
         "SYNC_WITH_VESNA",
         "FLASH",
         "STATE",
@@ -43,7 +159,7 @@ function commandSupported(c){
         "RESTART_APP"
     ];
 
-    app_commands = [
+    var app_commands = [
         "LINES",
         "SEC"
         //CONTIKI
@@ -60,42 +176,6 @@ function commandSupported(c){
     }
 }
 
-/* // OBSOLETE
-// Add device to the list of active devices
-function statelistAddDevice(dev, state){
-    
-    // Create new <p> element
-    var newP = document.createElement("p");
-    newP.appendChild(document.createTextNode(dev + ":" + state));
-    newP.setAttribute("id", dev);
-
-    // Append it to the div
-    var listdiv = document.getElementById("device_state_list");
-    listdiv.appendChild(newP);
-}
-
-// Update device state in the list
-function statelistUpdateDevice(dev, state){
-
-    // Find the <p> element with the ID of device
-    document.getElementById(dev).innerHTML = dev + ":" + state;
-}
-
-// Remove device from the list
-function statelistDeleteDevice(dev){
-    var element = document.getElementById(dev);
-    element.remove();
-}
-
-// Delete whole list
-function statelistRemove(){
-    var listdiv = document.getElementById("device_state_list");
-
-    while(listdiv.hasChildNodes()){
-        listdiv.removeChild(listdiv.firstChild);
-    }
-}
-*/
 
 // Websocket config (using jQuery on document ready)
 $(document).ready(function(){
@@ -114,6 +194,9 @@ $(document).ready(function(){
 // Prepare html
     // Delete old output logs
     $("#output_field").val("");
+
+    var node = new Nodes();
+    node.show_dev("LGTC55");
 
 // Handlers for events on client browser (using jQuery)
 
@@ -292,49 +375,3 @@ $(document).ready(function(){
 
 
 
-// ------------------------------------------------------------------------------------------------------------
-// Testbed tloris animation
-// ------------------------------------------------------------------------------------------------------------
-
-class Node {
-    //constructor(){
-    //}
-
-    show(n){
-        document.getElementById("node_" + n).style.display = "block";
-    }
-
-    update(n, color){
-        document.getElementById("node_" + n).style.color = color;
-    }
-}
-
-
-/*
-window.onload = function() {
-    let canvas = new Canvas()
-    canvas.add_dev(10,10)
-};
-
-
-class Canvas {
-
-    constructor(){
-        this.can = document.getElementById("tloris_canvas");
-        this.ctx = this.can.getContext("2d");
-
-        // Draw tloris image into canvas
-        var img = document.getElementById("source_img");
-        this.ctx.drawImage(img, 0, 0, 720, 550)
-    }
-
-    add_dev(x, y){
-        console.log("Add device to " + x + y)
-        this.ctx.beginPath();
-        this.ctx.arc(x,y,10,0,2*Math.PI);
-        this.ctx.stroke();
-        this.ctx.fillStyle = "#FF0000";
-        this.ctx.fill();
-    }
-} 
-*/
