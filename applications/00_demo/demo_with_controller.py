0c3e1f0f5cccc515b9c8970d63e87261294f8388
9morano <gregaa.morano@gmail.com>
2021-06-17 14:51:58 +0200

    Demo application

--- applications/00_demo/demo_with_controller.py
+++ applications/00_demo/demo_with_controller.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-from bluepy.btle import Scanner, DefaultDelegate
-import argparse
-import os
-import sys
 
-# Get the filename
-filename = "node_" + str(sys.arg[1]) + ".txt"
+import os, sys, time
+import logging
+from queue import Queue
+from datetime import datetime
+import argparse
+
+# Workaround to import files from parent dir
+sys.path.append("../../monitoring/")
+import controller_client
+
+from bluepy.btle import Scanner, DefaultDelegate
+
+
+# ----------------------------------------------------------------------------------------
+# EXPERIMENT DEFINITIONS AND CONFIGURATION
+# ----------------------------------------------------------------------------------------
+
+# DEFINITIONS
+LOG_LEVEL = logging.DEBUG
+
+ROUTER_HOSTNAME = "tcp://193.2.205.19:5562"
+SUBSCR_HOSTNAME = "tcp://193.2.205.19:5561"
+
+RESULTS_FILENAME = "node_results"
+LOGGING_FILENAME = "logger"
+
+# ENVIRONMENTAL VARIABLES
+# Device id should be given as argument at start of the script
+try:
+    LGTC_ID = sys.argv[1]
+    LGTC_ID = LGTC_ID.replace(" ", "")
+except:
+    print("No device name was given...going with default")
+    LGTC_ID = "xy"
+
+LGTC_NAME = "LGTC" + LGTC_ID
+RESULTS_FILENAME += ("_" + LGTC_ID + ".txt")
+LOGGING_FILENAME += ("_" + LGTC_ID + ".log")
+
 
 try:
 	APP_DURATION = int(os.environ['APP_DURATION_MIN'])
@@ -17,13 +49,33 @@ except:
 	APP_DURATION = 60
 
 
+# ----------------------------------------------------------------------------------------
+# FUNCTIONS to communicate with client script
+# ----------------------------------------------------------------------------------------
+def LGTC_send_sys_resp(self, state):
+	A_C_QUEUE.put(["-1", state])
+
+def LGTC_send_info_resp(self, state):
+	A_C_QUEUE.put(["0", state])
+
+def LGTC_send_cmd_resp(self, nbr, resp):
+	A_C_QUEUE.put([nbr, resp])
+
+def LGTC_rec_cmd(self):
+	return C_A_QUEUE.get()
+
+
+# ----------------------------------------------------------------------------------------
+# EXPERIMENT APPLICATION
+# ----------------------------------------------------------------------------------------
 class ScanDelegate(DefaultDelegate):
 	def __init__(self):
 		DefaultDelegate.__init__(self)
+
 	def handleDiscovery(self, dev, isNewDev, isNewData):
 
 		if isNewDev:
-			print("Discovered device", dev.addr, dev.rssi)
+			LGTC_send_info_resp("Discovered new device - %s" % dev.addr)
 			file.write("[" + str(datetime.now().time())+"]: ")
 			file.write("N " + str(dev.addr) + " RSSI" + str(dev.rssi) + "\n")
 		elif isNewData:
@@ -42,50 +94,69 @@ class ScanDelegate(DefaultDelegate):
 
 
 
-#parser = argparse.ArgumentParser(
-#    description="Store serial input into given file.",
-#    formatter_class=argparse.MetavarTypeHelpFormatter
-#)
-#parser.add_argument("-o", 
-#                    "--output", 
-#                    help="select file to store serial input", 
-#                    type=str,
-#                    required=False)
+# ----------------------------------------------------------------------------------------
+# MAIN
+# ----------------------------------------------------------------------------------------
+# Config logging module format for all scripts. Log level is defined in each submodule with var LOG_LEVEL.
+logging.basicConfig(format="%(asctime)s [%(levelname)7s]:[%(module)26s > %(funcName)16s() > %(lineno)3s] - %(message)s", level=LOG_LEVEL, filename=LOGGING_FILENAME)
+#logging.basicConfig(format="[%(levelname)5s:%(funcName)16s() > %(module)17s] %(message)s", level=LOG_LEVEL)
 
-#args = parser.parse_args()
+logging.info("Testing demo application for " + str(APP_DURATION) + " minutes on device " + LGTC_NAME + "!")
 
-#filename = "node_" + args.output + ".txt"
+# Create 2 queue for communication between threads
+# ZMQ CLIENT -> APPLICATION
+C_A_QUEUE = Queue()
+# APPLICATION -> ZMQ CLIENT
+A_C_QUEUE = Queue()
 
+# Start client thread (communication with controller)
+client_thread = controller_client.zmq_client_thread(A_C_QUEUE, C_A_QUEUE, LGTC_NAME, SUBSCR_HOSTNAME, ROUTER_HOSTNAME)
+client_thread.start()
 
-file = open(filename, mode="w", encoding = "ASCII")
+# Online
+LGTC_send_sys_resp("SYNCED_WITH_VESNA")
+
+# Prepare output file
+file = open(RESULTS_FILENAME, mode="w", encoding = "ASCII")
 file.write(str(datetime.now())+"\n")
 file.write("BLE measurements \n")
 file.write("N --> New device discovered \n D --> data received \n R --> rssi update \n")
 file.write("------------------------- \n")
 file.close()
+file = open(RESULTS_FILENAME, "a")
 
 # initialize scanner
 scr = Scanner().withDelegate(ScanDelegate())
 
+while(1):
+	if(not C_A_QUEUE.epmty()):
+		cmd = LGTC_rec_cmd()
 
-file = open(filename, "a")
+		if cmd[0] == "-1":
 
-# start scanning for 30 seconds
-scr.scan(timeout=APP_DURATION, passive=True) 
+			if cmd[1] == "EXIT":
+				break
+		else:
 
-
-
-
-
-
-
-
-
-
-
-
+			if cmd[1] == "START":
+				# start scanning for 30 seconds
+				LGTC_send_sys_resp("START")
+				scr.scan(timeout=APP_DURATION, passive=True)
+				LGTC_send_sys_resp("END")
 
 
+LGTC_send_sys_resp("EXIT")
+logging.info("Main thread stopped, trying to stop client thread.")
 
+# Wait for a second so client can finish its transmission
+time.sleep(1)
 
+# Notify zmq client thread to exit its operation and join until quit
+client_thread.stop()
+client_thread.join()
+
+logging.info("Exit!")
 # ---------------------------------------------------------------------------
+
+
+
---
+++ monitoring/controller_client.py
@@ -0,0 +1,232 @@
+# TODO:
+# Put zmq_client.py in this file as well?
+
+
+#!/usr/bin/python3
+import threading
+from queue import Queue
+
+import sys
+import os
+import logging
+import time
+
+from lib import zmq_client
+
+LOG_LEVEL = logging.DEBUG
+
+class zmq_client_thread(threading.Thread):
+
+    # ----------------------------------------------------------------------------------------
+    # INIT
+    # ----------------------------------------------------------------------------------------
+    def __init__(self, input_q, output_q, lgtc_id, subscriber, router):
+
+        threading.Thread.__init__(self)
+        self._is_thread_running = True
+
+        self.in_q = input_q
+        self.out_q = output_q
+
+        self.client = zmq_client.zmq_client(subscriber, router, lgtc_id)
+        self.__LGTC_STATE = "OFFLINE"
+
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(LOG_LEVEL)
+
+
+    # ----------------------------------------------------------------------------------------
+    # MAIN
+    # ----------------------------------------------------------------------------------------
+    def run(self):
+
+        # Sync with broker with timeout of 10 seconds
+        self.log.info("Sync with broker ... ")
+        self.client.transmit(["-1", "SYNC"])
+        if self.client.wait_ack("-1", 10) is False:
+            self.log.error("Couldn't synchronize with broker...")
+            self.out_q.put(["-1", "BROKER_DIED"])
+
+        # ------------------------------------------------------------------------------------
+        while self._is_thread_running:
+
+            # If there is a message from VESNA
+            # --------------------------------------------------------------------------------
+            if not self.in_q.empty():
+                response = self.in_q.get()
+                
+                # If the message is SYSTEM - new state
+                if response[0] == "-1":
+                    self.log.debug("New state: " + response[1])
+
+                    if response[1] == "START":
+                        self.LGTC_set_state("RUNNING")
+
+                    elif response[1] == "STOP":
+                        self.LGTC_set_state("STOPPED")
+
+                    elif response[1] == "END":
+                        self.LGTC_set_state("FINISHED")
+                    
+                    elif response[1] == "COMPILING":
+                        self.LGTC_set_state("COMPILING")
+
+                    elif response[1] == "FLASHED":
+                        self.LGTC_set_state("ONLINE")
+                    
+                    elif response[1] == "SYNCED_WITH_VESNA":
+                        self.LGTC_set_state("ONLINE")
+
+                    elif response[1] == "JOIN_DAG":
+                        self.LGTC_set_state("JOINED_NETWORK")
+
+                    elif response[1] == "EXIT_DAG":
+                        self.LGTC_set_state("EXITED_NETWORK")
+                        
+                    elif response[1] == "ROOT":
+                        self.LGTC_set_state("DAG_ROOT")
+
+                    elif response[1] == "VESNA_TIMEOUT":
+                        self.LGTC_set_state("TIMEOUT")
+
+                    elif response[1] == "COMPILE_ERR":
+                        self.LGTC_exit("COMPILE_ERR")
+                        break
+
+                    elif response[1] == "VESNA_ERR":
+                        self.LGTC_exit("VESNA_ERR")
+                        break
+                    
+                    else:
+                        self.LGTC_set_state("LGTC_WARNING")
+                        self.log.warning("+--> Unsupported state!")                    
+                
+                # If the message is CMD - experiment response
+                else:
+                    self.log.debug("Forwarding cmd response to broker...")
+                    self.client.transmit_async(response)
+            
+            # --------------------------------------------------------------------------------
+            # If there is some incoming commad from the controller broker
+            inp = self.client.check_input(0)
+            if inp:
+                msg_nbr, msg = self.client.receive_async(inp)
+
+                # If the message is not ACK
+                if msg_nbr:
+
+                    self.log.debug("Received command from broker: [" + msg_nbr + "] " + msg)
+                    # SYSTEM COMMANDS - application controll
+                    if msg_nbr == "-1":
+
+                        if msg == "STATE":
+                            self.client.transmit_async(["-1", self.LGTC_get_state()])
+
+                        elif msg == "EXIT":
+                            self.LGTC_set_state("OFFLINE")
+                            self.out_q.put([msg_nbr, msg])
+                            self.log.info("Closing client thread.")
+                            break
+                            
+                        else:
+                            self.out_q.put([msg_nbr, msg])
+
+                    # EXPERIMENT COMMANDS - experiment command
+                    else:
+                        self.out_q.put([msg_nbr, msg])
+
+
+            # --------------------------------------------------------------------------------
+            # If there is still some message that didn't receive ACK back from server, re send it
+            elif (len(self.client.waitingForAck) != 0):
+                self.client.send_retry()
+                #TODO self.out_q.put(["-1", "BROKER_DIED"])
+
+        # ------------------------------------------------------------------------------------
+        self.log.debug("Exiting client thread")
+        #self.client.close()
+
+
+    # ----------------------------------------------------------------------------------------
+    # END
+    # ----------------------------------------------------------------------------------------
+    def stop(self):
+        self._is_thread_running = False
+
+    
+
+
+    # ----------------------------------------------------------------------------------------
+    # OTHER FUNCTIONS
+    # ----------------------------------------------------------------------------------------
+
+    # Use in case of fatal errors in experiment app
+    def LGTC_exit(self, reason):
+        self.client.transmit(["-1", reason])
+        if self.client.wait_ack("-1", 3):
+            return True
+        else:
+            self.log.warning("No ACK from server while exiting...force exit.")
+            return False
+
+    # Get global variable
+    def LGTC_get_state(self):
+        return self.__LGTC_STATE
+
+    # Set global variable
+    def LGTC_set_state(self, state):
+        self.__LGTC_STATE = state
+        # Send new state to the server (WARNING: async method used...)
+        self.client.transmit_async(["-1", state])
+
+
+
+# This thread is designed for communication with controller_broker script - forwarding commands
+# and responses & updating LGTC states.
+
+# ----------------------------------------------------------------------------------------
+# POSSIBLE LGTC STATES
+# ----------------------------------------------------------------------------------------
+# --> ONLINE        - LGTC is online and ready
+# --> COMPILING     - LGTC is compiling the experiment application
+# --> RUNNING       - Experiment application is running
+# --> STOPPED       - User successfully stopped the experiment app
+# --> FINISHED      - Experiment application came to the end
+#
+# --> TIMEOUT       - VESNA is not responding for more than a minute
+# --> LGTC_WARNING  - Warning sign that something was not as expected
+# --> COMPILE_ERROR - Experiment application could not be compiled
+# --> VESNA_ERROR   - Problems with UART communication
+#
+#
+# ----------------------------------------------------------------------------------------
+# SUPPORTED COMMANDS
+# ----------------------------------------------------------------------------------------
+# Incoming commands must be formated as a list with 2 string arguments: message number 
+# and command itself (example: ["66", "SEC"]). Message number is used as a sequence
+# number, but if it is set to "-1", command represents SYSTEM COMMAND:
+#
+# --> SYSTEM COMMANDS - used for controll over the LGTC monitoring application
+#
+#       * STATE           - return the current state of monitoring application
+#       * EXIT            - exit monitoring application
+#       * ACK             - acknowledge packet sent as a response on every message
+#       * FLASH           - flash VESNA with experiment application
+#       * RESET           - reset VESNA device
+#       * BROKER_DIED     - if broker is not responding, inform serial monitor about it
+#       * SYNC            - used to synchronize LGTC with broker/server
+#
+# System commands are also used to update device state!
+#
+# --> EXPERIMENT COMMANDS - used for controll over the VESNA experiment application
+#
+#       * START           - start the experiment application
+#       * STOP            -
+#       * RESTART         - 
+#       * END             - when VESNA stops the experiment, this cmd is sent to broker
+#       * DURRATION       - return predefined duration of the application
+#       * LINES           - return the number of lines stored in measurement file
+#       * SEC             - return the number of elapsed seconds since the beginning of exp.
+#       TODO:
+#       Depend on Contiki-NG application
+
