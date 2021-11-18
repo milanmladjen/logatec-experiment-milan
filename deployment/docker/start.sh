9674ea7fd46e4d8fa961a2e7f8b8b218dd8a4bec
9morano <grega.morano@ijs.si>
2021-11-18 12:49:15 +0100

    Added technology parameter

--- deployment/docker/start.sh
+++ deployment/docker/start.sh
@@ -3,29 +3,18 @@
 if [ "$APP" = "" ]; then
   echo "No application given! Aborting!"
 else
+
   if [ "$OPTION" = "" ]; then
     echo "Option param is missing ... going with default."
     OPTION="none"
   fi
 
-  cd deployment/tasks/
+  if [ "$APP_DUR" = "" ]; then
+    echo "Application duration not defined ... going with default."
+    export APP_DUR="10"
+  fi
+
+  cd /root/logatec-experiment/deployment/tasks/
   pwd
   make "experiment_$OPTION"
 fi
-
-
-# -------------- OLD VERSION -------------------
-# Set test
-#if [ "$TARGET" = "" ]; then
-#  echo "Target missing!"
-#else
-#  if [[ "$TARGET": = *"agent"* ]]; then
-#    # Set serial device
-#    if [ -z "$VESNA_DEV" ]; then
-#      echo "Serial device missing!"
-#    fi
-#  fi
-#  cd deployment/tasks/
-#  pwd 
-#  make "$TARGET"
-#fi
